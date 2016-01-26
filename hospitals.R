44ab9993b07007b2fbf99ab705bdd49e8d6416c1
Antony Samarawickrema <antonysama@gmail.com>
2016-01-25 22:53:30 -0700

    Assignment

--- hospitals.R
+++ hospitals.R
@@ -6,5 +6,5 @@ d<-outcome[c(2,7,11,17,23)]
 names(d)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
 d[ order(d[,"state"], d[,"heart_attack"], d[ ,"name"]),]
 best <- function(state, outcome) {
-                             read.csv("outcome-of-care-measures.csv", 
-                                   na.strings = "Not Available", stringsAsFactors = F)
+                        read.csv("outcome-of-care-measures.csv", 
+                        na.strings = "Not Available", stringsAsFactors = F)
