16d638ea0bd6adc13ba22cab906b1b4215f99e78
Antony Samarawickrema <antonysama@gmail.com>
2016-01-27 00:13:05 -0700

    Assignment

--- hospitals.R
+++ hospitals.R
@@ -4,7 +4,10 @@ outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available"
 #hist(outcome[,11])#Q1
 d<-outcome[c(2,7,11,17,23)]
 names(d)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
+d[complete.cases(d[ ,3:5]),]
 d[ order(d[,"state"], d[,"heart_attack"], d[ ,"name"]),]
+d<-split(d,d$state)
+
 best <- function(state, outcome) {
                         read.csv("outcome-of-care-measures.csv", 
                         na.strings = "Not Available", stringsAsFactors = F)
