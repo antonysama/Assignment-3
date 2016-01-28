e71f1944ab53d58491c0b13db860d7409cf1313f
Antony Samarawickrema <antonysama@gmail.com>
2016-01-27 22:36:29 -0700

    Update hospitals.R

--- hospitals.R
+++ hospitals.R
@@ -1,7 +1,16 @@
 setwd("c:/Assignment 3")
 getwd()
 outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = F)
+#Q1
+hist(outcome[,11])#Q1
+#Q2
+outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
 #hist(outcome[,11])#Q1
+df<-outcome[c(2,7,11,17,23)]
+names(df)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
+region<-subset(df, state=="WY")
+region$name[which.min(region$heart_attack)]
+#Q3
 d<-outcome[c(2,7,11,17,23)]
 names(d)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
 d[complete.cases(d[ ,3:5]),]
