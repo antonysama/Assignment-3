372b44532b4612d3ef9a401f4bf775545fca9159
Antony Samarawickrema <antonysama@gmail.com>
2016-01-27 23:30:56 -0700

    Update hospitals.R

--- hospitals.R
+++ hospitals.R
@@ -8,14 +8,15 @@ outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available"
 #hist(outcome[,11])#Q1
 df<-outcome[c(2,7,11,17,23)]
 names(df)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
-region<-subset(df, state=="WY")
-region$name[which.min(region$heart_attack)]
+region<-subset(df, state=="WY")#WY is the region of interest
+region$name[which.min(region$heart_attack)]#heart_attack is the disease of interest
 #Q3
+outcome <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available")
 d<-outcome[c(2,7,11,17,23)]
 names(d)<-c("name","state", "heart_attack", "heart_failure", "pneumonia")
-d[complete.cases(d[ ,3:5]),]
-d[ order(d[,"state"], d[,"heart_attack"], d[ ,"name"]),]
-d<-split(d,d$state)
+region<-subset(d, state=="WY")#WY is the region of interest
+#region[complete.cases(region),]
+head(region[ order(region[,"state"], region[,"heart_attack"], region[ ,"name"]),], n=2)#n is the number of interest
 
 best <- function(state, outcome) {
                         read.csv("outcome-of-care-measures.csv", 
